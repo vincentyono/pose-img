c3757ce7034417982ba9030c603fa5c4084387aa
Vincent Yono <vincentyono@protonmail.com>
2024-10-29 05:59:23 +0700

    Add Styling and Routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "@fontsource/open-sans";
+import "./global.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Hello World!</h1>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <h1>Hello World!</h1>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
